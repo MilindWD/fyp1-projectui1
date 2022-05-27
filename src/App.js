379f6c07b428a017dc9ff34e50870353736a9b77
milindmgowda <43924876+milindmgowda@users.noreply.github.com>
2022-05-27 22:20:47 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,144 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React, { useEffect, useState } from "react";
+import {Col, Container, Dropdown, Row} from 'react-bootstrap';
 
-function App() {
+//colors
+//#181f2a : black
+
+
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+import getData from "./data";
+
+
+
+export default function App() {
+
+  const [model, setModel] = useState("Logistic Regression");
+  const [dataSet, setDataSet] = useState("SENTIMENT140");
+  const [attribute, setAttribute] = useState("accuracy");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(getData(model,  dataSet, attribute));
+  }, [model, dataSet, attribute]);
+
+  const handleModelChange = eventkey => {
+    console.log(eventkey);
+    setModel(eventkey);
+  }
+
+  const handleDataSetChange = eventkey => { 
+    setDataSet(eventkey);
+  }
+
+  const handleAttributeChange = eventkey => {
+    setAttribute(eventkey);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{alignItems: 'center'}}>
+    <Container>
+      <div style={{textAlign: 'center', padding: '1%', color: '#FFF205', fontFamily: 'Outfit'}}>
+        <h1 style={{fontSize: '3rem', fontWeight: '800'}}>Analysis of Political Commentary using Social Media
+        </h1>
+      </div>
+      <div style={{textAlign: 'center', padding: '1%', color: '#FFF205', fontFamily: 'Outfit', fontSize: '1.5rem'}}>
+        Below graph represents the results obtained
+      </div>
+      <div>
+      <Row style={{textAlign: 'center'}}> 
+        <Col xs={6}><Dropdown onSelect={handleModelChange}>
+        <Dropdown.Toggle variant="warning" id="dropdown-basic" style={{backgroundColor: '#FFF205', outline: 'none', borderRadius: '0', fontWeight: '600'}}>
+          {model}
+        </Dropdown.Toggle>
+      
+        <Dropdown.Menu >
+          <Dropdown.Item eventKey='Logistic Regression'>Logistic Regression</Dropdown.Item>
+          <Dropdown.Item eventKey='SVM' >SVM</Dropdown.Item>
+          <Dropdown.Item eventKey='DecisionTreeClassifier' >DecisionTree Classifier</Dropdown.Item>
+          <Dropdown.Item eventKey='LSTM'>LSTM</Dropdown.Item>
+          <Dropdown.Item eventKey='DNN'>DNN</Dropdown.Item>
+          <Dropdown.Item eventKey='GRU'>GRU</Dropdown.Item>
+          {dataSet!=='All'?<Dropdown.Item eventKey='All'>All</Dropdown.Item>:<></>}
+        </Dropdown.Menu>
+      </Dropdown></Col>
+      <Col xs={6}>
+      <Dropdown onSelect={handleDataSetChange}>
+      <Dropdown.Toggle variant="warning" id="dropdown-basic" style={{backgroundColor: '#FFF205', outline: 'none', borderRadius: '0', fontWeight: '600'}}>
+        {dataSet}
+      </Dropdown.Toggle>
+    
+      <Dropdown.Menu>
+        <Dropdown.Item eventKey='SENTIMENT140'>SENTIMENT140</Dropdown.Item>
+        <Dropdown.Item eventKey='IMDb50'>IMDb50</Dropdown.Item>
+        <Dropdown.Item eventKey='CAA_NRC'>CAA_NRC</Dropdown.Item>
+        {model!=='All'?<Dropdown.Item eventKey='All'>All</Dropdown.Item>:<></>}
+      </Dropdown.Menu>
+    </Dropdown>
+      </Col>
+      </Row>
+      </div>
+    </Container>
+    
+    <Container style={{height: '50vh', paddingTop: '10%'}} className="d-flex align-items-center justify-content-center text-center not-found-container"><LineChart
+    width={800}
+    height={400}
+    data={data}
+    margin={{
+      top: 5,
+      right: 30,
+      left: 20,
+      bottom: 5
+    }}
+  >
+    <CartesianGrid strokeDasharray="3 3" stroke="white"/>
+    <XAxis dataKey="name" stroke="white"/>
+    <YAxis stroke="white"/>
+    <Tooltip />
+    <Legend />
+    {model!=='All'&&dataSet!=='All'?<><Line type="monotone" dataKey="accuracy" stroke="#82ca9d" />
+    <Line type="monotone" dataKey="f1" stroke="red" />
+    <Line type="monotone" dataKey="precision" stroke="yellow" />
+    <Line type="monotone" dataKey="recall" stroke="white" /></>:<><Line type="monotone" dataKey="Bag of Words" stroke="#82ca9d" />
+    <Line type="monotone" dataKey="tfidf" stroke="red" />
+    <Line type="monotone" dataKey="GloVe" stroke="yellow" />
+    <Line type="monotone" dataKey="Word2Vec" stroke="white" />
+    <Line type="monotone" dataKey="LSTM" stroke="orange" />
+    </>}
+    
+    
+  </LineChart> 
+  
+  
+  
+  </Container>
+  <Container></Container>
+  {model!=='All'&&dataSet!=='All'?<></>:<><div style={{textAlign:'center', marginTop: '5%'}}>
+  <Dropdown onSelect={handleAttributeChange}>
+        <Dropdown.Toggle variant="warning" id="dropdown-basic" style={{backgroundColor: '#FFF205', outline: 'none', borderRadius: '0', fontWeight: '600'}}>
+          {attribute}
+        </Dropdown.Toggle>
+      
+        <Dropdown.Menu >
+          <Dropdown.Item eventKey='accuracy'>accuracy</Dropdown.Item>
+          <Dropdown.Item eventKey='f1' >f1</Dropdown.Item>
+          <Dropdown.Item eventKey='precision' >precision</Dropdown.Item>
+          <Dropdown.Item eventKey='recall'>recall</Dropdown.Item>
+
+        </Dropdown.Menu>
+      </Dropdown>
+  </div></>}
+  
+    </Container>
+    
   );
 }
-
-export default App;
